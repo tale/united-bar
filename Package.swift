48005011b19bf03495126b928e5489c8861bedb7
Aarnav Tale <git@tale.me>
2026-08-28 17:32:42 -0700

    chore: build against 26.0 SDK for liquid glass

--- Package.swift
+++ Package.swift
@@ -5,6 +5,17 @@ let package = Package(
     name: "UnitedBar",
     platforms: [.macOS(.v14)],
     targets: [
-        .executableTarget(name: "UnitedBar", path: "Sources/UnitedBar")
+        .executableTarget(
+            name: "UnitedBar",
+            path: "Sources/UnitedBar",
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xlinker", "-platform_version",
+                    "-Xlinker", "macos",
+                    "-Xlinker", "14.0",
+                    "-Xlinker", "26.0",
+                ])
+            ]
+        )
     ]
 )
